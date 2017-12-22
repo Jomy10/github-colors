0c7a25dd4125fb2c71719b8d33d6abcf90580797
LeeReindeer <reindeerlee.work@gmail.com>
2017-12-22 20:30:47 +0800

    Remove useless code

--- github-colors.go
+++ github-colors.go
@@ -16,10 +16,6 @@ type Lang struct {
 	color, url string
 }
 
-type T struct {
-	
-}
-
 func main()  {
 	m := GetGithubColors()
 	writeToJson(m)
@@ -33,13 +29,9 @@ func GetGithubColors() map[string]Lang {
 	langsMap := make(map[string]Lang)
 	fmt.Printf("Find %v languages in Github\n", len(m))
 	for name, attrs := range m {
-	  //fmt.Println("GetGithubColors3")
 		//fmt.Printf("%s: %v \n", name, attrs)
 		attrsMap, ok := attrs.(map[interface{}]interface{})
-		//fmt.Printf("isOk:%v ", ok)
-		//fmt.Println(attrsMap)
 		color, okk := attrsMap["color"]
-		//fmt.Printf("color: %v", color)
 		stringColor := fmt.Sprintf("%s", color)
 		//remove space from name
 		newName := strings.Replace(name, " ", "-", -1)
@@ -49,10 +41,6 @@ func GetGithubColors() map[string]Lang {
 				langsMap[newName] = Lang{"", newName}
 		}
 	}
-	//langsMap["Java"] = Lang{"86473f", "https://github.com/trending?l=java"}
-	//langsMap["Go"] = Lang{"58b2dc", "https://github.com/trending?l=go"}
-	//fmt.Println("colors:")
-	//fmt.Println(langsMap)
 	return langsMap
 }
 
@@ -65,46 +53,36 @@ func sliceOfKeys(m map[string]Lang) []string {
 }
 
 func downloadFile(filepath string, url string) (err error) {
-
   // Create the file
-  out, err := os.Create(filepath)
-  if err != nil  {
-    return err
-  }
+	out, err := os.Create(filepath)
+	checkErr(err)
   defer out.Close()
 
   // Get the data
-  resp, err := http.Get(url)
-  if err != nil {
-    return err
-  }
+	resp, err := http.Get(url)
+	checkErr(err)
   defer resp.Body.Close()
 
   // Writer the body to file
   _, err = io.Copy(out, resp.Body)
-  if err != nil  {
-    return err
-  }
+  checkErr(err)
 
   return nil
 }
 
 func readFile() map[string]interface{} {
-	// Create the file
+	//check exist. if not, create the file
 	if _, err := os.Stat("language.yml"); os.IsNotExist(err) {
 		fmt.Println("start downloding...")
 		downloadFile("language.yml", "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml")
 	}
 
 	m := make(map[string]interface{})
-	//fmt.Println(m)
-	//resp.Body.Read(ymlBytes)
 	ymlBytes, err := ioutil.ReadFile("language.yml")
 	checkErr(err)
-	//fmt.Printf("yml bytes[] :%s", string(ymlBytes))
+
 	err = yaml.Unmarshal(ymlBytes, m)
 	checkErr(err)
-	//fmt.Println(m)
 	return m
 }
 
@@ -124,36 +102,33 @@ func writeToReadme(m map[string]Lang)  {
 	fmt.Println("Write into README.md...")
 	var b []byte
 	s := "# Colors of programming languages on GitHub\n\n"
-	count := 0
+	s += "> Thanks [golang](golang.org) and [ymal](https://github.com/go-yaml/yaml).\n\n"
 	colorless := make(map[string]Lang)
 
 	//get slice of keys in map
 	keys := sliceOfKeys(m)
 	//sort keys
 	sort.Strings(keys)
+	//write color pic to file
 	for _, name := range keys {
 		lang := m[name]
-		count ++ 
 		if lang.color != "" {
-			//write color pic to file
 			//replace space -> -;remove #
 		  b = []byte(fmt.Sprintf("[![](http://via.placeholder.com/148x148/%s/ffffff&text=%s)](%s)", lang.color[1:], name, lang.url))
-		  if count % 6 == 0 {
-			  s += string(b) + "\n"
-		  } else {
-			  s += string(b)
-		  }	
+			s += string(b)
 		} else {
 			colorless[name] = lang
 		}
 	}
 	fmt.Printf("And %v languages are have no color\n", len(colorless))
+	
 	//write cloerless
 	s += "\nA few(lot) other languages don't have their own color on GitHub :( \n\n"
 	for name, lang := range colorless {
 		b = []byte(fmt.Sprintf("- [%s](%s)\n", name, lang.url))
 		s += string(b)
 	}
+
 	s += "\nCurious about all this? Check `ABOUT.md`.\n"
 	outByte := []byte(s)
 	ioutil.WriteFile("README.md", outByte, 0644)
